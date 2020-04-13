def0628e16e28774ec2e0487bd5f3c39fabfbc91
Matthias Jacobi <mail@majaco.de>
2020-04-13 22:08:28 +0200

    set php7.4 as standard in composer.json and added new method in request

--- src/framework/request/uri/Uri.php
+++ src/framework/request/uri/Uri.php
@@ -4,74 +4,88 @@
  *
  * @author     Andreas Habel <mail@ahabel.de>
  * @copyright  Conperience GmbH, Andreas Habel and contributors
- *
  */
 
 namespace Lucille\Request;
 
 use Lucille\Exceptions\UriPartIndexOutOfBoundsException;
-    
+
 /**
  * Class Uri
  *
  * @package Lucille\Request
  */
-class Uri {
-    
+class Uri
+{
+
     /**
      * @var string
      */
     private $uri;
-    
+
     /**
      * @var string
      */
     private $path;
-    
+
     /**
      * @param string $uri Original request Uri
      */
-    public function __construct(string $uri) {
-        $this->uri = trim($uri);
+    public function __construct(string $uri)
+    {
+        $this->uri  = trim($uri);
         $this->path = parse_url($this->uri, PHP_URL_PATH);
         if ($this->path !== '/') {
             $this->path = rtrim($this->path, '/');
         }
     }
-    
+
     /**
      * @return string
      */
-    public function asString(): string {
+    public function asString(): string
+    {
         return $this->path;
     }
-    
+
     /**
      * @return string
      */
-    public function originUriAsString(): string {
+    public function originUriAsString(): string
+    {
         return $this->uri;
     }
-    
+
     /**
-     * @param int $index URI path index value
-     * @return string
      * @throws UriPartIndexOutOfBoundsException
+     * @return string
+     * @param int $index URI path index value
      */
-    public function getPart(int $index): UriPart {
+    public function getPart(int $index): UriPart
+    {
         $tmp = explode('/', trim(urldecode($this->asString()), '/'));
-        
+
         if ($index >= count($tmp) || $index < 0) {
             throw new UriPartIndexOutOfBoundsException("No uri segment found for given index value");
         }
         return new UriPart($tmp[$index]);
     }
 
+    public function getPartAsStringOrNull(int $index): ?string
+    {
+        try {
+            $this->getPart($index)->asString();
+        } catch (UriPartIndexOutOfBoundsException $e) {
+        }
+        return null;
+    }
+
     /**
      * @param int $index URI path index value
      * @return bool
      */
-    public function hasPart(int $index): bool {
+    public function hasPart(int $index): bool
+    {
         $tmp = explode('/', trim(urldecode($this->asString()), '/'));
 
         if ($index >= count($tmp) || $index < 0) {
@@ -84,28 +98,31 @@ class Uri {
      * @param Uri $uri Uri reference
      * @return bool
      */
-    public function isEqual(Uri $uri): bool {
+    public function isEqual(Uri $uri): bool
+    {
         return $this->asString() === $uri->asString();
     }
-    
+
     /**
      * @param Uri $uri Uri reference
      * @return bool
      */
-    public function beginsWith(Uri $uri): bool {
+    public function beginsWith(Uri $uri): bool
+    {
         if (substr($this->path, 0, strlen($uri->asString())) === $uri->asString()) {
             return true;
         }
         return false;
     }
-    
+
     /**
      * @param UriRegEx $regexPattern RegEx pattern
      * @return bool
      */
-    public function matchesRegEx(UriRegEx $regexPattern): bool {
+    public function matchesRegEx(UriRegEx $regexPattern): bool
+    {
         $rc = preg_match($regexPattern->asString(), $this->path, $m);
         return ($rc > 0 ? true : false);
     }
-    
+
 }
